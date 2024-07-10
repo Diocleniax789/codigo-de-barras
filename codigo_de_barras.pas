b0404b2d6b4e2e5ea10dc98637cc4e8e4006ca33
Diocleniax789 <julioramdom123@gmail.com>
2024-07-10 18:30:12 -0300

    se creo el programa y se iniciarion los primeros progresos en el que se creo un menu y la asignacion de un codigo de empresa validando que sea de tres digitos y tambien la opcion de poder repetir el proceso

---
+++ codigo_de_barras.pas
@@ -0,0 +1,57 @@
+PROGRAM codigos_de_barras;
+USES crt;
+
+VAR
+   codigo_empresa: integer;
+
+FUNCTION valida_codigo_de_empresa(): integer;
+VAR
+  cod_empre: integer;
+  BEGIN
+  REPEAT
+  textcolor(white);
+  write('>>> Ingrese codigo de empresa <unicamente de tres digitos>: ');
+  readln(cod_empre);
+  IF (cod_empre < 100) OR (cod_empre > 999) THEN
+   BEGIN
+   textcolor(lightred);
+   writeln();
+   writeln('==========================================================');
+   writeln('X POR FAVOR. INGRESE UN NUMERO DE TRES CIFRAS NUEVAMENTE X');
+   writeln('==========================================================');
+   writeln();
+   END;
+  UNTIL (cod_empre >= 100) AND (cod_empre <= 999);
+  valida_codigo_de_empresa:= cod_empre;
+  END;
+
+PROCEDURE genera_codigo_de_barras;
+VAR
+ codigo_empresa: integer;
+ BEGIN
+ writeln();
+ codigo_empresa:= valida_codigo_de_empresa;
+ END;
+
+PROCEDURE menu_principal;
+VAR
+ opcion: integer;
+ BEGIN
+ REPEAT
+ writeln('1. Generar codigo de barras');
+ writeln('2. Salir');
+ writeln();
+ write('Seleccione opcion: ');
+ readln(opcion);
+ CASE opcion OF
+      1:BEGIN
+        clrscr;
+        genera_codigo_de_barras;
+        END;
+ END;
+ UNTIL (opcion = 2);
+ END;
+
+BEGIN
+menu_principal;
+END.
